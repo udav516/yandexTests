2b16b51b2a46e08a0307a83ec7f25643539cdc38
udav516 <eldar.udav516@yandex.ru>
2023-08-06 20:39:28 +0300

    add Task E

--- test_1.py
+++ test_1.py
@@ -70,3 +70,37 @@ parentheses.sort()
 
 for p in parentheses:
     print(p)
+
+
+# Task E
+def checkAnagram(s1, s2):
+    charCount = {}
+    for char in s1:
+        if char in charCount:
+            charCount[char] += 1
+        else:
+            charCount[char] = 1
+
+    for char in s2:
+        if char in charCount:
+            charCount[char] -= 1
+            if charCount[char] < 0:
+                return False
+        else:
+            return False
+
+    for count in charCount.values():
+        if count != 0:
+            return False
+
+    return True
+
+
+s1 = input()
+s2 = input()
+
+result = checkAnagram(s1, s2)
+if result:
+    print(1)
+else:
+    print(0)
