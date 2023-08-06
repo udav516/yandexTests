18135d0fba0f8f71d5f0e81f3394625fca52252c
udav516 <eldar.udav516@yandex.ru>
2023-08-06 19:32:16 +0300

    add Task B

--- test_1.py
+++ test_1.py
@@ -1,4 +1,4 @@
-# Task 1
+# Task A
 def jewels_and_stones(J: str, S: str) -> int:
     jewels = set(J)
     count = 0
@@ -14,3 +14,21 @@ S = input()
 result = jewels_and_stones(J, S)
 
 print(result)
+
+# Task B
+n = int(input())
+
+max_len = 0
+cur_len = 0
+
+for _ in range(n):
+    num = int(input())
+    if num == 1:
+        cur_len += 1
+    else:
+        max_len = max(max_len, cur_len)
+        cur_len = 0
+
+max_len = max(max_len, cur_len)
+
+print(max_len)
